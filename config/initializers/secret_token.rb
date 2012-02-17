6dfb8d020827ba73d7e157187b5048f13788d838
Brux Voight <bruxvoight@sbcglobal.net>
2012-02-17 10:00:19 -0600

    New Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoToolLikeAnOldTool::Application.config.secret_token = 'a654d87b916825a6dfe0bd9577491ec6439fba50d48ddb10ec85a8f8ac085560b7a8213a9e0e4834281db25c97ebc6f6501259e5b5e46e458459a4542ac1ae0d'
